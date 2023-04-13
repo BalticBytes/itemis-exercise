824ec4febeb3c7513fe1a299b3b597cc306004bf
Christopher Starck <christopher.starck@googlemail.com>
2023-04-13 16:17:22 +0200

    basic processing into id-by-numeral map

--- src/app.go
+++ src/app.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	// "github.com/ttacon/chalk"
 	"github.com/urfave/cli/v2"
@@ -29,13 +31,48 @@ func run(c *cli.Context) error {
 	}
 
 	input := args.Get(0)
-	output := processInput(input)
-	fmt.Printf("%s", output)
+	processInput(input)
+	// output := processInput(input)
+	// fmt.Printf("%s", output)
 
 	return nil
 }
 
 func processInput(input string) string {
-	// TODO
+
+	pattern := []string{
+		"^([a-zA-Z]+) is ([IVXLCDM]+)$", // assignment
+	}
+
+	id_by_numeral := map[string]string{
+		"I": "",
+		"V": "",
+		"X": "",
+		"L": "",
+		"C": "",
+		"D": "",
+		"M": "",
+	}
+
+	for _, line := range strings.Split(input, "\n") {
+		for i := 0; i < len(pattern); i++ {
+			pattern := pattern[i]
+			re := regexp.MustCompile(pattern)
+
+			matches := re.FindStringSubmatch(line)
+
+			if matches != nil {
+				identifier := matches[1]
+				romanNumeral := matches[2]
+				// fmt.Printf("%s: %s\n", identifier, romanNumeral)
+				id_by_numeral[romanNumeral] = identifier
+			}
+		}
+	}
+
+	for k, v := range id_by_numeral {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
 	return input
 }
