dad0cc323f6a053a68818dddecea54f233477bb5
Christopher Starck <christopher.starck@googlemail.com>
2023-04-15 10:47:35 +0200

    refactor: question type into own file

--- src/app.go
+++ src/app.go
@@ -35,28 +35,6 @@ var (
 	}
 )
 
-// There is no "true" enum type so keyword is just a string
-type Question struct {
-	keyword string // much | many
-	amount  string
-	unit    *string // optional
-}
-
-func (q Question) String() string {
-	unit := "N/A"
-	if q.unit != nil {
-		unit = *q.unit
-	}
-	return fmt.Sprintf("{How %s is %s %s ?}", q.keyword, q.amount, unit)
-}
-
-// returns the sum of an intergalactic
-func (q Question) calculate(m map[string]string, n map[string]float64) int {
-	_, i := translate(m, q.amount)
-
-	return int(float64(i) * n[*q.unit])
-}
-
 // translates a string from intergalactic transaction to roman numerals.
 func translate(numeralByIntergalacticInput map[string]string, intergalacticInput string) (string, int) {
 	parts := strings.Split(intergalacticInput, " ")
