beed2c2da89095339ad489c10e7b9067d3fba00d
Christopher Starck <christopher.starck@googlemail.com>
2023-04-15 10:34:30 +0200

    gofmt + merge main and run methods

--- src/app.go
+++ src/app.go
@@ -85,28 +85,26 @@ func translate(numeralByIntergalacticInput map[string]string, intergalacticInput
 
 func main() {
 	app := &cli.App{
-		Name:   "exercise",
-		Usage:  "Input => Output",
-		Action: run,
+		Name:  "exercise",
+		Usage: "Input => Output",
+		Action: func(c *cli.Context) error {
+			args := c.Args()
+
+			if args.Len() == 0 {
+				return fmt.Errorf("missing input argument")
+			}
+
+			input := args.Get(0)
+			output := processInput(input)
+			fmt.Printf("%s", output)
+
+			return nil
+		},
 	}
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func run(c *cli.Context) error {
-	args := c.Args()
-
-	if args.Len() == 0 {
-		return fmt.Errorf("missing input argument")
-	}
-
-	input := args.Get(0)
-	output := processInput(input)
-	fmt.Printf("%s", output)
-
-	return nil
 }
 
 func processInput(input string) string {
