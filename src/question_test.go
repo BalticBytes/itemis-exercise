eedb1e998ce2dcde1267ce5da261bcb58114d48c
Christopher Starck <christopher.starck@googlemail.com>
2023-04-16 13:56:36 +0200

    generate unit tests for question.go with gotests

---
+++ src/question_test.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewQuestion(t *testing.T) {
+	type args struct {
+		line string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Question
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewQuestion(tt.args.line); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewQuestion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestQuestion_calculate(t *testing.T) {
+	type args struct {
+		m map[string]string
+		n map[string]float64
+	}
+	tests := []struct {
+		name    string
+		q       Question
+		args    args
+		wantSum int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotSum := tt.q.calculate(tt.args.m, tt.args.n); gotSum != tt.wantSum {
+				t.Errorf("Question.calculate() = %v, want %v", gotSum, tt.wantSum)
+			}
+		})
+	}
+}
+
+func TestQuestion_answer(t *testing.T) {
+	type args struct {
+		numeralById    map[string]string
+		unitCostByUnit map[string]float64
+	}
+	tests := []struct {
+		name       string
+		q          Question
+		args       args
+		wantAnswer string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotAnswer := tt.q.answer(tt.args.numeralById, tt.args.unitCostByUnit); gotAnswer != tt.wantAnswer {
+				t.Errorf("Question.answer() = %v, want %v", gotAnswer, tt.wantAnswer)
+			}
+		})
+	}
+}
