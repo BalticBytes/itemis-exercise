53f928e1312f82badfae08b147c94822baa64b3e
Christopher Starck <christopher.starck@googlemail.com>
2023-04-17 14:48:06 +0200

    add more possible whitespace to question regex

--- src/utils.go
+++ src/utils.go
@@ -5,8 +5,8 @@ import "regexp"
 const (
 	numeralPattern = "^[ \t]*([a-zA-Z]+)[ \t]+is[ \t]+([IVXLCDM]+)[ \t]*$"
 	creditsPattern = "^[ \t]*([a-zA-Z ]+)[ \t]+([a-zA-Z]+)[ \t]+is[ \t]+([0-9]+)[ \t]+[cC][rR][eE][dD][iI][tT][sS][ \t]*$"
-	howMuchPattern = "^[ \t]*how[ \t]+much[ \t]+is[ \t]+([a-zA-Z ]+)[ \t]*\\?$"
-	howManyPattern = "^[ \t]*how[ \t]+many[ \t]+[cC][rR][eE][dD][iI][tT][sS][ \t]+is[ \t]+([a-zA-Z ]+)[ \t]+([a-zA-Z]+)[ \t]*\\?$"
+	howMuchPattern = "^[ \t]*how[ \t]+much[ \t]+is[ \t]+([a-zA-Z ]+)[ \t]*\\?[ \t]*$"
+	howManyPattern = "^[ \t]*how[ \t]+many[ \t]+[cC][rR][eE][dD][iI][tT][sS][ \t]+is[ \t]+([a-zA-Z ]+)[ \t]+([a-zA-Z]+)[ \t]*\\?[ \t]*$"
 )
 
 var (
